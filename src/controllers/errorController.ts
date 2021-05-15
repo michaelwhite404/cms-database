edb477543bf7d4f369f6497644478823ab712a9e
Michael White <michaelwhite404@gmail.com>
2021-05-15 13:26:44 -0400

    Fixed errorController bugs

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,104 +1,103 @@
 import { NextFunction, Request, Response } from "express";
-import mongoose, { CastError} from "mongoose"
-import { MongoError } from "mongodb"
+import mongoose, { CastError } from "mongoose";
+import { MongoError } from "mongodb";
 import AppError from "../utils/appError";
 
 type ValidationError = mongoose.Error.ValidationError;
 
 const handleCastErrorDB = (err: CastError): AppError => {
-  const message = `Invalid ${err.path}: ${err.value}.`;
-  return new AppError(message, 400);
+	const message = `Invalid ${err.path}: ${err.value}.`;
+	return new AppError(message, 400);
 };
 
 const handleDuplicateFieldsDB = (err: MongoError): AppError => {
-  const value = err.errmsg!.match(/(["'])(\\?.)*?\1/)![0];
+	const value = err.errmsg!.match(/(["'])(\\?.)*?\1/)![0];
 
-  const message = `Duplicate field value: ${value}. Please use another value!`;
-  return new AppError(message, 400);
+	const message = `Duplicate field value: ${value}. Please use another value!`;
+	return new AppError(message, 400);
 };
 const handleValidationErrorDB = (err: ValidationError): AppError => {
-  const errors = Object.values(err.errors).map((el) => el.message);
+	const errors = Object.values(err.errors).map((el) => el.message);
 
-  const message = `Invalid input data. ${errors.join(". ")}`;
-  return new AppError(message, 400);
+	const message = `Invalid input data. ${errors.join(". ")}`;
+	return new AppError(message, 400);
 };
 
-const handleJWTError = () =>
-  new AppError("Invalid Token. Please log in again!", 401);
+const handleJWTError = () => new AppError("Invalid Token. Please log in again!", 401);
 
 const sendErrorDev = (err: AppError, req: Request, res: Response) => {
-  // A) API
-  if (req.originalUrl.startsWith("/api")) {
-    return res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
-  }
+	// A) API
+	if (req.originalUrl.startsWith("/api")) {
+		return res.status(err.statusCode).json({
+			status: err.status,
+			error: err,
+			message: err.message,
+			stack: err.stack,
+		});
+	}
 
-  return res.status(err.statusCode).render("error", {
-    title: err.statusCode + " Error",
-    message: err.message,
-  });
+	return res.status(err.statusCode).render("error", {
+		title: err.statusCode + " Error",
+		message: err.message,
+	});
 };
 
 const sendErrorProd = (err: AppError, req: Request, res: Response) => {
-  // A) API
-  if (req.originalUrl.startsWith("/api")) {
-    // A) Operational, trusted error: send message to client
-    if (err.isOperational) {
-      return res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-      });
-    }
-    // B) Programming or other unknown error: don't leak error details
-    // 1) Log error
-    console.error("ERROR 💥", err);
-    // 2) Send generic message
-    return res.status(500).json({
-      title: err.statusCode + " Error",
-      message: err.message,
-    });
-  }
+	// A) API
+	if (req.originalUrl.startsWith("/api")) {
+		// A) Operational, trusted error: send message to client
+		if (err.isOperational) {
+			return res.status(err.statusCode).json({
+				status: err.status,
+				message: err.message,
+			});
+		}
+		// B) Programming or other unknown error: don't leak error details
+		// 1) Log error
+		console.error("ERROR 💥", err);
+		// 2) Send generic message
+		return res.status(500).json({
+			title: err.statusCode + " Error",
+			message: err.message,
+		});
+	}
 
-  // B) RENDERED WEBSITE
-  // A) Operational, trusted error: send message to client
-  if (err.isOperational) {
-    return res.status(err.statusCode).render("error", {
-      title: err.statusCode + " Error",
-      message: err.message,
-    });
-  }
-  // B) Programming or other unknown error: don't leak error details
-  // 1) Log error
-  console.error("ERROR 💥", err);
-  // 2) Send generic message
-  return res.status(err.statusCode).render("error", {
-    title: "Something went wrong!",
-    msg: "Please try again later.",
-  });
+	// B) RENDERED WEBSITE
+	// A) Operational, trusted error: send message to client
+	if (err.isOperational) {
+		return res.status(err.statusCode).render("error", {
+			title: err.statusCode + " Error",
+			message: err.message,
+		});
+	}
+	// B) Programming or other unknown error: don't leak error details
+	// 1) Log error
+	console.error("ERROR 💥", err);
+	// 2) Send generic message
+	return res.status(err.statusCode).render("error", {
+		title: "Something went wrong!",
+		msg: "Please try again later.",
+	});
 };
 
 export default (err: AppError, req: Request, res: Response, _: NextFunction) => {
-  // console.log(err.stack);
+	// console.log(err.stack);
 
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, req, res);
-  } else if (process.env.NODE_ENV === "production") {
-    let error = Object.assign(err);
-    let appError = <AppError>{};
+	if (process.env.NODE_ENV === "development") {
+		sendErrorDev(err, req, res);
+	} else if (process.env.NODE_ENV === "production") {
+		let error = Object.assign(err);
+		let appError = <AppError>{ ...error };
+		appError.message = err.message;
 
-    if (error.name === "CastError") appError = handleCastErrorDB(error);
-    if (error.code === 11000) appError = handleDuplicateFieldsDB(error);
-    if (error.name === "ValidationError")
-     appError = handleValidationErrorDB(error);
-    if (error.name === "JsonWebTokenError") appError = handleJWTError();
+		if (error.name === "CastError") appError = handleCastErrorDB(error);
+		if (error.code === 11000) appError = handleDuplicateFieldsDB(error);
+		if (error.name === "ValidationError") appError = handleValidationErrorDB(error);
+		if (error.name === "JsonWebTokenError") appError = handleJWTError();
 
-    sendErrorProd(appError, req, res);
-  }
+		sendErrorProd(appError, req, res);
+	}
 };
